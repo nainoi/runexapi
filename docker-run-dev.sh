ba16695f942122379774987ab2e5ddd2a244ee5c
suchart.j <suchart@kkumail.com>
2020-08-21 11:56:46 +0700

    Rename docker run container file.

---
+++ docker-run-dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+NAME=runex-api
+docker stop $NAME;
+docker rm -f $NAME;
+
+docker run -itd --name $NAME -v $(pwd):/app -p 3008:3006 registry.thinkdev.app/think/runex/runexapi:dev;
+docker logs -f $NAME
