15759a49595d77d6f480337a2ddf58ee6e7b5b62
suchart.j <suchart@kkumail.com>
2020-09-12 12:32:39 +0700

    Update docker deploy

--- docker-deploy-uat.sh
+++ docker-deploy-uat.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-docker image build -t registry.thinkdev.app/think/runex/runexapi:dev -f Dockerfile.dev .
+GIT_COMMIT=$(git log -1 --format=%h)
 
-# docker push registry.thinkdev.app/think/runex/runexapi:dev;
-# scp docker-run-uat.sh root@128.199.163.81:/root \
-# && ssh root@128.199.163.81 "sudo sh docker-run-uat.sh && exit" \
-docker save registry.thinkdev.app/think/runex/runexapi:dev > runexapi.tar \
-&& scp runexapi.tar root@128.199.163.81:/root \
-&& scp docker-run-uat.sh root@128.199.163.81:/root \
-&& ssh root@128.199.163.81 "sudo docker load < runexapi.tar && sudo sh docker-run-uat.sh && exit" \
-&& rm runexapi.tar
+docker push registry.thinkdev.app/think/runex/runexapi:$GIT_COMMIT;
+scp docker-run-uat.sh root@128.199.163.81:/root \
+&& ssh root@128.199.163.81 "sudo sh docker-run-uat.sh && exit" \
 
 
-# scp docker-run-on-cloud.sh ols-user@203.150.107.41:/home/ols-user \
-# && ssh ols-user@203.150.107.41 "sudo docker load < kp-callsceen-note-dev.tar && sudo sh docker-run-on-cloud.sh && exit" \
--- docker-run-uat.sh
+++ docker-run-uat.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+GIT_COMMIT=$(git log -1 --format=%h)
 NAME=runex-api
 docker stop $NAME;
 docker rm -f $NAME;
 
-docker run -itd --name $NAME  -p 3008:3006 registry.thinkdev.app/think/runex/runexapi:dev;
+docker run -itd --name $NAME  -p 3008:3006 registry.thinkdev.app/think/runex/runexapi:$GIT_COMMIT;
 docker logs -f $NAME
