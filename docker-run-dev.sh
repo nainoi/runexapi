ed24adee1049d7ea9cc3dde775584cec29e00da5
suchart.j <suchart@kkumail.com>
2020-09-12 12:32:03 +0700

    Update docker build

--- docker-run-dev.sh
+++ docker-run-dev.sh
@@ -3,5 +3,5 @@ NAME=runex-api
 docker stop $NAME;
 docker rm -f $NAME;
 
-docker run -itd --name $NAME -v $(pwd):/app -p 3008:3006 registry.thinkdev.app/think/runex/runexapi:dev;
+docker run -itd --name $NAME -v $(pwd):/app -p 3008:3006 --memory="1g" --cpus="1" registry.thinkdev.app/think/runex/runexapi:dev;
 docker logs -f $NAME
