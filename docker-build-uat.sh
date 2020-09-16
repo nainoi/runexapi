2940dad26717daa5fc54e755b8d92f316bf4d4b2
suthisakch <suthisak.ch@hotmail.com>
2020-09-16 22:42:21 +0700

    add docker file uat and edit build file

--- docker-build-uat.sh
+++ docker-build-uat.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 GIT_COMMIT=$(git log -1 --format=%h)
-docker image build -t registry.thinkdev.app/think/runex/runexapi:$GIT_COMMIT -f Dockerfile .
+docker image build -t registry.thinkdev.app/think/runex/runexapi:$GIT_COMMIT -f Dockerfile.uat .
