1304e6e4ccd27bde63b7041eb92685eb1d3c5691
suchart.j <suchart@kkumail.com>
2020-11-05 09:53:59 +0700

    Edit container name and path.

--- docker-run-prd.sh
+++ docker-run-prd.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-NAME=runex-api
+NAME=api.runex.co
 docker stop $NAME;
 docker rm -f $NAME;
 
 PRIVATE_IP=$(hostname -I | cut -d' ' -f3)
-docker run -itd --name $NAME  -p $PRIVATE_IP:3008:3006 -v /hosting/www/runex-api/uploads:/root/upload think-runex-api;
+docker run -itd --name $NAME  -p $PRIVATE_IP:3008:3006 -v /hosting/www/api.runex.co/uploads:/root/upload think-runex-api;
 docker logs -f $NAME
