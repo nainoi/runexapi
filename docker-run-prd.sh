c58d9fceba6cced47d6492ce9533970ff8e5e449
suchart.j <suchart@kkumail.com>
2020-11-05 09:39:51 +0700

    Add security for publish only private ip.

--- docker-run-prd.sh
+++ docker-run-prd.sh
@@ -3,5 +3,6 @@ NAME=runex-api
 docker stop $NAME;
 docker rm -f $NAME;
 
+IP=$(hostname -I | cut -d' ' -f3)
 docker run -itd --name $NAME  -p 3008:3006 -v ~/runex-api/uploads:/root/upload think-runex-api;
 docker logs -f $NAME
