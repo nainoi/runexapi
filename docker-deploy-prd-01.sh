21d012cf0a24a8563702c383eebbf9f1702a4921
suchart.j <suchart@kkumail.com>
2020-11-05 09:47:18 +0700

    Update deploy for private ip.

--- docker-deploy-prd-01.sh
+++ docker-deploy-prd-01.sh
@@ -5,9 +5,5 @@ docker image build -t think-runex-api -f Dockerfile .
 docker save think-runex-api > runexapi.tar \
 && scp runexapi.tar root@$SERVER_IP:/root \
 && scp docker-run-prd.sh root@$SERVER_IP:/root \
-&& ssh root@$SERVER_IP "sudo docker load < runexapi.tar && sudo sh docker-run-prd.sh && exit" \
+&& ssh root@$SERVER_IP "docker load < runexapi.tar && sh docker-run-prd.sh && exit" \
 && rm runexapi.tar
-
-
-# scp docker-run-on-cloud.sh ols-user@203.150.107.41:/home/ols-user \
-# && ssh ols-user@203.150.107.41 "sudo docker load < kp-callsceen-note-dev.tar && sudo sh docker-run-on-cloud.sh && exit" \
